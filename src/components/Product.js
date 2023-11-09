6b3ab085aa1f03873124a000072c0e9e12d4eb9d
Utkarsh Mishra <um952000@gmail.com>
2023-11-09 13:28:11 +0530

    jhguiujh

--- src/components/Product.js
+++ src/components/Product.js
@@ -59,10 +59,9 @@ const Product = ({ item, provider, account, dappazon, togglePop }) => {
 
           <p>
             {item.description}
-
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima rem, iusto,
-            consectetur inventore quod soluta quos qui assumenda aperiam, eveniet doloribus
-            commodi error modi eaque! Iure repudiandae temporibus ex? Optio!
+             
+             Best product to buy......
+          
           </p>
         </div>
 
